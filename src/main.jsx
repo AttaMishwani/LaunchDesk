ad1f6798e85f95841442ff9eb38a5e35fb5229cb
AttaMishwani <atta.rehmanmishwani@gmail.com>
2025-05-19 19:01:04 +0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/freelancerbox">
           <App />
           <ToastContainer />
         </BrowserRouter>
