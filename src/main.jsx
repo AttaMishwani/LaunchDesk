11c2476a637d713c75b92d02bf5bd8024e3c0d6a
nickaykays <nick.aykays@gmail.com>
2025-07-20 00:06:35 +0500

    fixed routing

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ createRoot(document.getElementById("root")).render(
         <PersistGate loading={null} persistor={persistor}>
           <BrowserRouter
             basename={
-              import.meta.env.MODE === "production" ? "/freelancerbox" : "/"
+              import.meta.env.MODE === "production" ? "/launchdesk" : "/"
             }
           >
             <App />
